ca5e0d57bf95f54859224a49b0362dac6bfefddf
mauratee <thomaura@gmail.com>
2025-01-07 12:55:00 -0500

    Debug apartments in db

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def show_apartments():
     apartments = db.execute('SELECT * from apartments').fetchall()
     db.close()
     # print(apartments)
-    return f"apartments: {apartments[0]}"
+    return f"apartments: {apartments[0]['housenumber']}"
     # return render_template('apartments.html', apartments=apartments)
 
 
