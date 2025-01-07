9bcd696f18b2273385dbb2e15f1426b62d16846d
mauratee <thomaura@gmail.com>
2025-01-07 13:02:31 -0500

    debug apartments.html

--- server.py
+++ server.py
@@ -51,8 +51,8 @@ def show_apartments():
     apartments = db.execute('SELECT * from apartments').fetchall()
     db.close()
     # print(apartments)
-    return f"apartments: {apartments[0]['housenumber']} {apartments[0]['street']}, {apartments[0]['borough']}"
-    # return render_template('apartments.html', apartments=apartments)
+    # return f"apartments: {apartments[0]['housenumber']} {apartments[0]['street']}, {apartments[0]['borough']}"
+    return render_template('apartments.html', apartments=apartments)
 
 
 
