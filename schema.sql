d4dd56fe46add486e2e8910ea20340f457ed1e3b
mauratee <thomaura@gmail.com>
2025-01-08 15:06:59 -0500

    Add records db table

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS apartments;
+DROP TABLE IF EXISTS records;
 
 CREATE TABLE apartments (
     id INTEGER PRIMARY KEY,
@@ -7,4 +8,17 @@ CREATE TABLE apartments (
     borough TEXT NOT NULL,
     unitnumber TEXT,
     postalcode TEXT
+);
+
+CREATE TABLE records (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year TEXT NOT NULL,
+    status TEXT NOT NULL,
+    filing_date TEXT,
+    legal_rent TEXT,
+    preferential_rent TEXT,
+    actual_rent TEXT,
+    reasons_difference TEXT,
+    lease_dates TEXT,
+    FOREIGN KEY (apartment_id) REFERENCES apartments (id)
 );
