2a6164a947ed128c7973cbf5fd337926db158a9d
mauratee <thomaura@gmail.com>
2025-01-09 15:43:21 -0500

    debug apartment by id page

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ def all_apartments():
 @app.route("/apartments/<apartment_id>")
 def show_apartment(apartment_id):
     db = get_db()
-    records = db.execute('SELECT * FROM records WHERE apartment_id = ?', (apartment_id)).fetchall()
+    records = db.execute('SELECT * FROM records WHERE apartment_id = ?', (apartment_id,)).fetchall()
     db.close()
     return render_template('records.html', records=records)
 
