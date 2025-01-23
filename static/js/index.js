6f003a9c5b0f505a655957aec4f270a085372bcd
mauratee <thomaura@gmail.com>
2025-01-23 18:10:26 -0500

    add js functions to index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,57 @@
 
 "use strict";
 
-alert("JS is connected");
-console.log("JavaScript is working!");
+function getValue() {
+    var input = document.getElementById("search");
+    var input_value = input.value;
+    return input_value
+}
+
+async function sendValue() {
+    var user_input = getValue();
+    const url = "https://geosearch.planninglabs.nyc/v2/autocomplete?text="+user_input;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        return json
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+async function parseData() {
+    var address_data = await sendValue();
+    const address_labels = []
+    address_data.features.forEach(element => {
+        address_labels.push(element.properties.label)
+    });
+    return address_labels
+}
+
+async function createDropdown() {
+    var address_list = await parseData()
+    if (address_list) {
+        var datalist = document.getElementById("addresses");
+        datalist.replaceChildren();
+        console.log(address_list)
+        address_list.forEach(address => {
+            var datalist = document.getElementById("addresses");
+            const option = document.createElement('option');
+            option.value = address;
+            datalist.appendChild(option);
+        });
+    }
+}
+
+var text_box = document.getElementById("search");
+var addresses = document.getElementById("addresses");
+text_box.addEventListener('input', function(event) {
+    addresses.innerHTML = '';
+    createDropdown();
+});
 
