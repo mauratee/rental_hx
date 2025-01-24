d1136c4d0410b96174f0cd80bbbe3ef3dfda04ae
mauratee <thomaura@gmail.com>
2025-01-23 19:00:07 -0500

    text api key env variable

--- static/js/records.js
+++ static/js/records.js
@@ -1,5 +1,8 @@
 
 "use strict";
 
-alert(`Hello ${process.env.MPBX_API_KEY}`)
-console.log(`Hello ${process.env.MPBX_API_KEY}`)
+alert("JS is connected");
+console.log("JS is connected");
+
+// alert(`Hello ${process.env.MPBX_API_KEY}`);
+console.log(`Hello ${process.env.MPBX_API_KEY}`);
