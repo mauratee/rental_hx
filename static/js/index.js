5eef6a806727d381349f8b8075750f710b36480e
mauratee <thomaura@gmail.com>
2025-01-24 17:01:22 -0500

    test address element highlight

--- static/js/index.js
+++ static/js/index.js
@@ -46,7 +46,7 @@ async function createDropdown() {
             const option = document.createElement('li');
             const span = document.createElement('span');
             span.textContent = address;
-            span.id = "address"
+            span.id = "address";
             // option.value = address;
             option.appendChild(span)
             datalist.appendChild(option);
