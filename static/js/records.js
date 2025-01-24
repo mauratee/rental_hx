d8f5df109151e216bbcc4e5f382905ace37cbe82
mauratee <thomaura@gmail.com>
2025-01-24 13:25:03 -0500

    change map position in css

--- static/js/records.js
+++ static/js/records.js
@@ -1,20 +1,12 @@
 
 "use strict";
 
-// alert("JS is connected");
-console.log("JS is connected");
-
-// alert(`Hello ${process.env.MPBX_API_KEY}`);
-console.log("Hello ${process.env.MPBX_API_KEY}");
-// console.log(process.env.MPBX_API_KEY);
-
 
 fetch('/get_token')
     .then(response => response.json())
     .then(data => {
         var mapboxToken = data.mapbox_token;
-        // console.log(mapboxToken)
-        // Use mapboxToken in your Mapbox GL JS code
+     
         mapboxgl.accessToken = mapboxToken;
         const map = new mapboxgl.Map({
             container: 'map',
