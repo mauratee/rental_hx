7171be2af8bff2b3f8afe1c5cffb21cb1122428a
mauratee <thomaura@gmail.com>
2025-01-23 20:13:15 -0500

    test map

--- static/js/records.js
+++ static/js/records.js
@@ -13,6 +13,13 @@ fetch('/get_token')
     .then(response => response.json())
     .then(data => {
         var mapboxToken = data.mapbox_token;
-        console.log(mapboxToken)
+        // console.log(mapboxToken)
         // Use mapboxToken in your Mapbox GL JS code
+        mapboxgl.accessToken = mapboxToken;
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+            zoom: 9 // starting zoom
+        });
     });
+
